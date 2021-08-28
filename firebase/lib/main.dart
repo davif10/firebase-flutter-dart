f07d139ca1c034ed5ec9be9146baa461466f971e
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-08-28 19:47:01 -0300

    Salvando e atualizando dados

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -1,16 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   //Inicializar Firebase
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore.instance.collection("usuarios")
-  .document("pontuacao")
-  .setData({"Davi":"250", "Ana": "190"});
+  Firestore db = Firestore.instance;
+  /*
+  db.collection("usuarios")
+  .document("001")
+  .setData({
+    "nome": "Davi",
+    "idade": "31"
+  });
+  */
 
-  runApp(MyApp());
-}
+  // DocumentReference ref = await db.collection("noticias")
+  // .add(
+  //     {
+  //       "titulo": "Criada moeda virtual!",
+  //       "descricao": "Texto de exemplo..."
+  //     }
+  // );
+  // print("Item salvo: ${ref.documentID}");
+
+  db.collection("noticias")
+      .document("spMKcEn3wO1D7eXtQ5ZN")
+      .setData(
+      {
+        "titulo": "Criada moeda virtual!",
+        "descricao": "Texto de exemplo alterado..."
+      });
+
+runApp(MyApp());}
+
 class MyApp extends StatelessWidget {
 
   @override
