071ee10c972838ec071ef545133459cd40f28324
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-08-28 20:55:41 -0300

    Aplicando filtros básicos para a pesquisa e pesquisando um texto

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -6,34 +6,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   Firestore db = Firestore.instance;
-  //db.collection("noticias").document("spMKcEn3wO1D7eXtQ5ZN").delete();
-  // db.collection("usuarios")
-  //     .document("001")
-  //     .setData({
-  //   "nome":"Davi",
-  //   "idade": "26"
-  // });
+  var pesquisa = "a";
+  QuerySnapshot snapshot = await db.collection("usuarios")
+      //.where("nome", isEqualTo:"julia")
+      // .where("idade", isLessThan: "21")
+      // .where("idade", isGreaterThan: "18")
+      // .orderBy("idade", descending: true)
+      //.limit(1)
+      .where("nome", isGreaterThanOrEqualTo: pesquisa)
+      .where("nome", isLessThanOrEqualTo: pesquisa +"\uf8ff")
+      .getDocuments();
 
-  // DocumentSnapshot snapshot = await db.collection("usuarios")
-  //     .document("001")
-  //     .get();
-  // print("Dados: ${snapshot.data}");
+  for(DocumentSnapshot item in snapshot.documents){
+    print("Filtro nome: ${item.data}");
+  }
 
-  // QuerySnapshot snapshot = await db.collection("usuarios")
-  //     .getDocuments();
-  //print("Dados usuários: ${snapshot.documents.toString()}");
-
-  // for(DocumentSnapshot item in snapshot.documents){
-  //   //print("Dados usuários: ${item.data}");
-  //   print("Dados usuários: Nome => ${item.data["nome"]} Idade => ${item.data["idade"]}");
-  // }
-
-  db.collection("usuarios")
-      .snapshots().listen((event) {
-        for(DocumentSnapshot item in event.documents){
-          print("Dados: ${item.data}");
-        }
-  });
 
 runApp(MyApp());}
 
