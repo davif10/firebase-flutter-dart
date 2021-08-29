ba3bf0455f46ebc8734cc5de4111befa88866a6d
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-08-29 14:12:42 -0300

    -Realizando cadastro e verificação de usuário logado. -Logar e deslogar usuário.

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -1,34 +1,85 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   //Inicializar Firebase
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore db = Firestore.instance;
-  var pesquisa = "a";
-  QuerySnapshot snapshot = await db.collection("usuarios")
-      //.where("nome", isEqualTo:"julia")
-      // .where("idade", isLessThan: "21")
-      // .where("idade", isGreaterThan: "18")
-      // .orderBy("idade", descending: true)
-      //.limit(1)
-      .where("nome", isGreaterThanOrEqualTo: pesquisa)
-      .where("nome", isLessThanOrEqualTo: pesquisa +"\uf8ff")
-      .getDocuments();
-
-  for(DocumentSnapshot item in snapshot.documents){
-    print("Filtro nome: ${item.data}");
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String email = "davi2@gmail.com";
+  String senha = "123456";
+  // auth.createUserWithEmailAndPassword(
+  //     email: email, password: senha
+  // ).then((firebaseUser){
+  //     print("Novo usuário: Sucesso!! Email: ${firebaseUser.email}");
+  // }).catchError((erro){
+  //   print("Novo usuário: erro $erro");
+  // });
+  FirebaseUser usuarioAtual = await auth.currentUser();
+  if(usuarioAtual != null){//Logado
+    print("Usuario Atual logado email: ${usuarioAtual.email}");
+  }else{//Deslogado
+    print("Usuario deslogado.");
   }
 
+  runApp(
+      MaterialApp(
+        home: MyApp(),
+      )
+  );}
 
-runApp(MyApp());}
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    cadastroLoginUsuario();
+  }
+
+  cadastroLoginUsuario() async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String email = "davi2@gmail.com";
+    String senha = "123456";
+    // auth.createUserWithEmailAndPassword(
+    //     email: email, password: senha
+    // ).then((firebaseUser){
+    //     print("Novo usuário: Sucesso!! Email: ${firebaseUser.email}");
+    // }).catchError((erro){
+    //   print("Novo usuário: erro $erro");
+    // });
+    //auth.signOut(); //Deslogar
+
+    auth.signInWithEmailAndPassword(
+        email: email,
+        password: senha).then((firebaseUser){
+      print("Logar usuário: Sucesso!! Email: ${firebaseUser.email}");
+    }).catchError((erro){
+      print("Logar usuário: Erro: $erro");
+    });
+
+    // FirebaseUser usuarioAtual = await auth.currentUser();
+    // if(usuarioAtual != null){//Logado
+    //   print("Usuario Atual logado email: ${usuarioAtual.email}");
+    // }else{//Deslogado
+    //   print("Usuario deslogado.");
+    // }
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase"),
+      ),
+      body: Container(
+
+      ),
+    );
   }
 }
 
