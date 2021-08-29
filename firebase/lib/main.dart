9905a6b9e9f56356e5c2198933ced71fc2c1e7e4
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-08-29 16:51:20 -0300

    -Realizando upload de imagens -Verificando status de upload e baixando imagens -Atualizando bibliotecas e alterando o código.

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -2,30 +2,15 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   //Inicializar Firebase
   WidgetsFlutterBinding.ensureInitialized();
-
-  FirebaseAuth auth = FirebaseAuth.instance;
-  String email = "davi2@gmail.com";
-  String senha = "123456";
-  // auth.createUserWithEmailAndPassword(
-  //     email: email, password: senha
-  // ).then((firebaseUser){
-  //     print("Novo usuário: Sucesso!! Email: ${firebaseUser.email}");
-  // }).catchError((erro){
-  //   print("Novo usuário: erro $erro");
-  // });
-  FirebaseUser usuarioAtual = await auth.currentUser();
-  if(usuarioAtual != null){//Logado
-    print("Usuario Atual logado email: ${usuarioAtual.email}");
-  }else{//Deslogado
-    print("Usuario deslogado.");
-  }
-
+  await Firebase.initializeApp();
   runApp(
       MaterialApp(
         home: Home(),
@@ -40,6 +25,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   File _imagem;
+  String _statusUpload = "Upload não iniciado";
+  String _urlImagem = null;
   @override
   void initState() {
     super.initState();
@@ -53,26 +40,26 @@ class _HomeState extends State<Home> {
     // auth.createUserWithEmailAndPassword(
     //     email: email, password: senha
     // ).then((firebaseUser){
-    //     print("Novo usuário: Sucesso!! Email: ${firebaseUser.email}");
+    //     print("Novo usuário: Sucesso!! Email: ${firebaseUser.user.email}");
     // }).catchError((erro){
     //   print("Novo usuário: erro $erro");
     // });
-    //auth.signOut(); //Deslogar
+    // auth.signOut(); //Deslogar
 
     auth.signInWithEmailAndPassword(
         email: email,
         password: senha).then((firebaseUser){
-      print("Logar usuário: Sucesso!! Email: ${firebaseUser.email}");
+      print("Logar usuário: Sucesso!! Email: ${firebaseUser.user.email}");
     }).catchError((erro){
       print("Logar usuário: Erro: $erro");
     });
 
-    // FirebaseUser usuarioAtual = await auth.currentUser();
-    // if(usuarioAtual != null){//Logado
-    //   print("Usuario Atual logado email: ${usuarioAtual.email}");
-    // }else{//Deslogado
-    //   print("Usuario deslogado.");
-    // }
+    User usuarioAtual = await auth.currentUser;
+    if(usuarioAtual != null){//Logado
+      print("Usuario Atual logado email: ${usuarioAtual.email}");
+    }else{//Deslogado
+      print("Usuario deslogado.");
+    }
   }
   
   _recuperarImagem(bool daCamera) async {
@@ -87,6 +74,53 @@ class _HomeState extends State<Home> {
     });
   }
 
+  _uploadImagem() async{
+    if(_imagem != null){
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference pastaRaiz = storage.ref();
+      Reference arquivo = pastaRaiz
+          .child("fotos")
+          .child("foto2.jpg");
+
+      //Fazer upload
+      UploadTask task = arquivo.putFile(_imagem);
+
+      //Controlar progresso do Upload
+      task.snapshotEvents.listen((TaskSnapshot snapshot) {
+        if(snapshot.state == TaskState.running){
+          setState(() {
+            _statusUpload = "Em progresso...";
+          });
+
+        }else if(snapshot.state == TaskState.success){
+          _recuperarUrlImagem(snapshot);
+          setState(() {
+            _statusUpload = "Upload realizado com sucesso!";
+          });
+
+        }else if(snapshot.state == TaskState.error){
+          setState(() {
+            _statusUpload = "Erro ao fazer upload.";
+          });
+        }
+      });
+
+      //Recuperar url da imagem
+      // task.onComplete.then((snapshot){
+      //   _recuperarUrlImagem(snapshot);
+      // });
+    }
+  }
+
+  _recuperarUrlImagem(TaskSnapshot snapshot) async{
+    String url = await snapshot.ref.getDownloadURL();
+    print("Resultado URL: $url");
+
+    setState(() {
+      _urlImagem = url;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -98,6 +132,7 @@ class _HomeState extends State<Home> {
         child: Center(
           child: Column(
             children: [
+              Text(_statusUpload),
               ElevatedButton(
                   child: Text("Camera"),
                   onPressed: (){
@@ -112,7 +147,19 @@ class _HomeState extends State<Home> {
               ),
               _imagem == null
               ? Container()
-                  : Image.file(_imagem)
+                  : Image.file(_imagem),
+
+              _imagem == null
+                  ? Container()
+                  : ElevatedButton(
+                child: Text("Upload Storage"),
+                onPressed: (){
+                  _uploadImagem();
+                },
+              ),
+              _urlImagem == null
+              ? Container()
+              : Image.network(_urlImagem)
             ],
           ),
         ),
